043a1448615ead0cef6f59ae6a4dd972784166bd
ASHISH KUMAR <40332678+ashishnnnnn@users.noreply.github.com>
2021-08-18 14:40:41 +0530

    feat - Input OutPut Processing Done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var button_translate = document.querySelector("#btn-translate")
 var txtinput = document.querySelector("#txt-input");
+var outputdiv = document.querySelector("#output");
+
+
 
 function clickEventHandler(){
-    console.log("Clicked");
-    console.log("Input Values",txtinput.value)
+    outputdiv.innerText = "ASDFGH "+txtinput.value;
 }
 
 
